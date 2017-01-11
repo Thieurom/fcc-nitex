980842ce4b6cb7f8e7d4b3c1f0182ea707688bd6
Doan Le Thieu <thieurom@gmail.com>
2017-01-11 22:03:03 +0700

    Add view for homepage

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 'use strict'
 
+// Depedencies
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
 
-
+// App instance
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('It works');
-});
+// Setup middlewares
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Routes
+app.use('/', require('./routes/home'));
+
+// app.get('/', (req, res) => {
+//   res.send('It works');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
